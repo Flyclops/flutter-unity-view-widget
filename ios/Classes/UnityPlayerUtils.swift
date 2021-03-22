50c28e2a3fa697714f62c83704ce370920daedcd
Jake O'Brien <neuronrain@gmail.com>
2021-03-22 15:58:11 -0400

    Fix for unityDidUnload callback not firing
    Made the unityDidUnload callback available to obj-c and conform to the expected swift signature so it could be called by UnityFramework. Removed state handling from unload method as it is now handled in the callback.

--- ios/Classes/UnityPlayerUtils.swift
+++ ios/Classes/UnityPlayerUtils.swift
@@ -156,8 +156,9 @@ var sharedApplication: UIApplication?
             self.ufw?.unregisterFrameworkListener(self)
         }
     }
-    
-    private func unityDidUnload(notification: Notification?) {
+
+    @objc
+    public func unityDidUnload(_ notification: Notification!) {
         unregisterUnityListener()
         self.ufw = nil
         self._isUnityReady = false
@@ -223,10 +224,6 @@ var sharedApplication: UIApplication?
     // Unoad unity player
     func unload() {
         self.ufw?.unloadApplication()
-        unregisterUnityListener()
-        self.ufw = nil
-        self._isUnityReady = false
-        self._isUnityLoaded = false
     }
 
     func isUnityLoaded() -> Bool {
